22dc11ad94520e6b544a57ceab592dd707ab3c70
Olivier <olivier.coujan@gmail.com>
2021-06-14 18:33:18 +0200

    Initial commit

---
+++ src/main/java/com/example/ProjetDomotiqueAPI/ProjetDomotiqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjetDomotiqueAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDomotiqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDomotiqueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjetDomotiqueAPI/ProjetDomotiqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetDomotiqueAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDomotiqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
